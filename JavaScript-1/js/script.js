2ed84a4a8b610f03e9c4c9f75496c4899ddb6afa
Sergiy Burlaka <sergiy.burlaka@adwokat.kiev.ua>
2017-05-06 18:16:30 +0300

    JS Home work 1-2

--- JavaScript-1/js/script.js
+++ JavaScript-1/js/script.js
@@ -1,8 +1,81 @@
-var arr = [1, 2,3,4,5];
-	if (arr[1] < 5){
-		alert(arr[1] + " < 5");
+"use strict";
+/*
+function reverseString(text) {
+	var arr = [];
+	for (var i = text.length - 1; i >= 0 ; i--){
+		arr.push(text.charAt(i));
 	}
+		console.log( arr.join(''));
+}
+
+reverseString(prompt('Введите текст', ''));
 
 
+function findLongestWord(findWord) {
+	var text = findWord.split(' ');
+	var nomber = 0;
+	var index = 0;
+	for (var i = 0;  i < text.length  ;i++){
+		if (text[i].length > nomber) {
+			nomber = text[i].length;
+			index = i;
+			console.log(index);	
+		}
+	}
+	var result = text[index] +':'+ nomber;
+	console.log(result);
+}
+
+findLongestWord(prompt('Введите текст через провел', ''));
 
 
+function factorialize(number) {
+	var factorial = 1;
+	for(var i = 1 ; i  <= number ; i++){
+		factorial *= i;
+	}
+// return factorial;
+	console.log('Факториал часла: '+factorial);
+}
+
+factorialize(prompt("Факториал часла:", ''));
+
+
+function pow(base, exponent) {
+	var summ = 1;
+ if (base > 0){	
+	for (var i=0;  exponent > i; i++){
+			summ *= base;
+		}
+		console.log(summ);
+	//	return summ;
+	}else{
+	console.log("0");
+	}
+}
+
+pow(prompt("Введите число:", ''), prompt("Введите степень:", ''));
+*/
+// Добавление пользователей в базу
+var firstName = [];
+var nomberName = prompt("Сколько пользователей добавить в базу:? ", '');
+
+for (var i =0;i < nomberName; i++){
+	firstName.push(prompt("Введите имя для добавления:", ''));
+}
+
+var searchName = prompt("Введите имя для Поиска:", '');
+
+console.log(firstName);
+top:
+for (var i=0;i < firstName.length ; i++){
+	if (firstName[i].toLowerCase() === searchName.toLowerCase()){
+		console.log(firstName[i].toUpperCase() +', вы успешно вошли!!!');
+		break;
+	}
+	else if (firstName[i].toLowerCase() !== searchName.toLowerCase()) {
+		alert('Пользователя ' + searchName.toUpperCase() + ' не существует');
+		continue top;
+	
+}
+	}
