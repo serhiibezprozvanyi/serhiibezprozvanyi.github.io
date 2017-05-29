6d8cdcd0eaa254abf020543809cb7d6ea7b27bec
Sergiy Burlaka <sergiy.burlaka@adwokat.kiev.ua>
2017-05-30 01:00:56 +0300

    home work 5-6 edit error

--- JavaScript-5-6/js/script.js
+++ JavaScript-5-6/js/script.js
@@ -1,60 +1,63 @@
+"use strict"
 var startDate =  0;
 var date;
+var hours;
 var start;
 var pauseDate = 0;
 var isActive = false;
 var int = null;
-var start = ["00","00","00","000"]	
+var start = ['00','00','00','000'];
 var topdate;
 function html (){
 
 var div = document.createElement('div');
 var p = document.createElement('p');
-p.setAttribute("id", "time");
+p.setAttribute('id', 'time');
 p.innerHTML = start.join(':');
 div.appendChild(p);
 return document.body.appendChild(div);
 }
 
 function hmtlTimer(){
-	var date = Date.now();
- 	start = ["00","00","00","000"];
+	date = Date.now();
+ 	start = ['00','00','00','000'];
 	topdate =  new Date(date - startDate + pauseDate);
-	start[3] = topdate.getMilliseconds()
+	start[3] = topdate.getMilliseconds();
 	start[2] = (topdate.getSeconds() <10) ? '0'+ topdate.getSeconds(): topdate.getSeconds();
 	start[1] = (topdate.getMinutes() <10) ? '0'+ topdate.getMinutes(): topdate.getMinutes();
 	hours = topdate.getHours() - topdate.getTimezoneOffset()/-60;
-	start[0] = (hours <10) ? '0'+ hours: hours;		
-	return document.getElementById('time').innerHTML = start.join(':');
-};
+	start[0] = (hours <10) ? '0'+ hours: hours;
+	document.getElementById('time').innerHTML = start.join(':');
+}
 
 function creatingButton(textButton, id) {
 	var button = document.createElement('button');
 	button.setAttribute('type','submit');
-	button.setAttribute("id", id);
-    button.setAttribute("value", "#");
-    button.innerHTML=textButton;
+	button.setAttribute('id', id);
+  button.setAttribute('value', '#');
+  button.innerHTML=textButton;
 	console.log(button);
-	return button;	
-};
+	return button;
+}
 
-var stop = creatingButton("Стоп", "stop");
-var split = creatingButton("split", "split");
-var st = creatingButton("Старт", "start");
+var stop = creatingButton('Стоп', 'stop');
+var split = creatingButton('split', 'split');
+var st = creatingButton('Старт', 'start');
 
 
 function setTimer(){
 		startDate  = Date.now();
 	return startDate;
-       };
-     
+       }
+
 function stopTimer() {
-	 start = ["00","00","00","000"];
+ start = ['00','00','00','000'];
      clearInterval(int);
+	 pauseDate = 0;
      document.getElementById('time').innerHTML = start.join(':');
 	 st.innerHTML=("Старт");
 	 isActive = false;
-     };
+ }
 
 function updateTimer(){
 	st.innerHTML=("Старт");
@@ -68,13 +71,13 @@ st.addEventListener('click', function() {
 	  if (isActive === false){
 		  setTimer();
 		  int = setInterval(hmtlTimer,1);
-          isActive = true;	 
+          isActive = true;
 		  st.innerHTML=("Пауза");
 	  }
 	else{
 		updateTimer();
         isActive = false;
-	}  
+	}
 });
 
 split.addEventListener('click', function() {
@@ -85,7 +88,6 @@ stop.addEventListener('click', function() {
 });
 
 html();
-//document.body.appendChild(html());
 document.body.appendChild(st);
 document.body.appendChild(split);
 document.body.appendChild(stop);
