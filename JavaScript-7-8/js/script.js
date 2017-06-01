e0f21bfb3c9d3dc4f7d92137293a96bf163cf871
Sergiy Burlaka <sergiy.burlaka@adwokat.kiev.ua>
2017-06-02 00:53:46 +0300

    home work 7-8 edit erorr

--- JavaScript-7-8/js/script.js
+++ JavaScript-7-8/js/script.js
@@ -9,3 +9,18 @@ $(document).ready(function(){
         $(tab).fadeIn(300);
     });
 });
+function simple_tooltip(name){
+$('input').each(function(i){
+$("fieldset").append("<p class='"+name+"' id='"+name+i+"'>"+$(this).attr('title')+"</p>");
+var my_tooltip = $("#"+name+i);
+
+$(this).removeAttr("title").focus(function(){
+my_tooltip.css({opacity:0.8, display:"block"}).fadeIn(400);
+}).focusout(function(){
+my_tooltip.fadeOut(400);
+});
+});
+}
+$(document).ready(function(){
+simple_tooltip("tooltip");
+});
