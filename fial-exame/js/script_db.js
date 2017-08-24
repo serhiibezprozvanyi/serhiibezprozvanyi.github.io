5ff73be5f88b53a77f962dd66c4bebc62ff04d82
Serhii Bezprozvanyi <bzserg@gmail.com>
2017-08-25 00:46:15 +0300

    fial exame

---
+++ fial-exame/js/script_db.js
@@ -0,0 +1,68 @@
+(function () {
+
+	let	myDataL =[
+		{
+			id:1,
+			src:["img/desktop/1.jpg","img/tablet/1.jpg","img/mobile/1.jpg"],
+			title:"Morbi velit risus",
+			text:"Nulla venenatis tempor dui in molestie. Nulla quis dictum purus, sit amet porttitor est."
+		},
+		{
+			id:2,
+			src:["img/desktop/2.jpg","img/tablet/2.jpg","img/mobile/2.jpg"],
+			title:"Morbi velit risus",
+			text:"Nulla venenatis tempor dui in molestie. Nulla quis dictum purus, sit amet porttitor est."
+		},
+		{
+			id:3,
+			src:["img/desktop/3.jpg","img/tablet/3.jpg","img/mobile/3.jpg"],
+			title:"Sed leo enim, condimentum",
+			text:"Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis."
+		}
+];
+	let	myDataM =[
+		{
+			id:1,
+			src:["img/desktop/4.jpg","img/tablet/4.jpg","img/mobile/4.jpg"],
+			title:"Morbi velit risus",
+			text:"Nulla venenatis tempor dui in molestie. Nulla quis dictum purus, sit amet porttitor est."
+		},
+		{
+			id:2,
+			src:["img/desktop/5.jpg","img/tablet/5.jpg","img/mobile/5.jpg"],
+			title:"Morbi velit risus",
+			text:"Nulla venenatis tempor dui in molestie. Nulla quis dictum purus, sit amet porttitor est."
+		},
+		{
+			id:3,
+			src:["img/desktop/6.jpg","img/tablet/6.jpg","img/mobile/6.jpg"],
+			title:"Sed leo enim, condimentum",
+			text:"Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis."
+		}
+	];
+	let	myDataR =[
+		{
+			id:1,
+			src:["img/desktop/7.jpg","img/tablet/7.jpg","img/mobile/7.jpg"],
+			title:"Morbi velit risus",
+			text:"Nulla venenatis tempor dui in molestie. Nulla quis dictum purus, sit amet porttitor est."
+		},
+		{
+			id:2,
+			src:["img/desktop/8.jpg","img/tablet/8.jpg","img/mobile/8.jpg"],
+			title:"Morbi velit risus",
+			text:"Nulla venenatis tempor dui in molestie. Nulla quis dictum purus, sit amet porttitor est."
+		},
+		{
+			id:3,
+			src:["img/desktop/9.jpg","img/tablet/9.jpg","img/mobile/9.jpg"],
+			title:"Sed leo enim, condimentum",
+			text:"Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis."
+		}
+
+	];
+	
+
+
+
+
